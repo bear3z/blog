b3b5af7e35913e8cdd6897b4c9c14ae0e15e41a6
Eddy <46402041+bear3z@users.noreply.github.com>
2024-02-21 15:39:36 +0800

    Feat: Waves component

---
+++ src/lib/components/organisms/Waves.story.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '$lib/scss/global.scss';
+	import Waves from './Waves.svelte';
+	
+	import type { Hst } from '@histoire/plugin-svelte';
+	
+	export let Hst: Hst;
+</script>
+
+<svelte:component
+	this={Hst.Story}
+	title="Organisms/Waves"
+	layout={{ type: 'single', iframe: true }}
+>
+	<Waves />
+</svelte:component>
