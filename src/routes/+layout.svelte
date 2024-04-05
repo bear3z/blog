349d019c9a06c96456e50ca3ab2402d2f6295b28
Eddy <46402041+bear3z@users.noreply.github.com>
2024-04-06 06:24:58 +0800

    Chore: update main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,51 @@
-<script>
+<script lang="ts">
 	import '$lib/scss/global.scss';
+	import { beforeNavigate, afterNavigate, onNavigate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	let root: HTMLElement;
+	onMount(async () => {
+		root = document.getElementsByTagName('html')[0];
+		root.classList.add('smooth-scroll');
+
+		if (window) {
+			// @ts-ignore
+			const { plausible } = window;
+			if (plausible) {
+				if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+					plausible('pageview', { props: { theme_preference: 'Dark' } });
+				} else if (
+					window.matchMedia &&
+					window.matchMedia('(prefers-color-scheme: light)').matches
+				) {
+					plausible('pageview', { props: { theme_preference: 'Light' } });
+				} else {
+					plausible('pageview', { props: { theme_preference: 'None' } });
+				}
+			}
+		}
+	});
+
+	beforeNavigate(() => {
+		root.classList.remove('smooth-scroll');
+	});
+	
+	afterNavigate(() => {
+		root.classList.add('smooth-scroll');
+	});
+
+	onNavigate((navigation) => {
+		// @ts-ignore
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-ignore
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <slot />
